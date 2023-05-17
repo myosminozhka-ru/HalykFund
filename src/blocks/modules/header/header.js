7cb90a2d86cc12fe96df8ec772935e72f3f88f76
arsosmi <sheraliev@myosminozhka.ru>
2023-05-17 18:06:20 +0600

    index page start

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -3,6 +3,13 @@ export default class Header {
     this.header = document.querySelector('.header');
     this.burger = document.querySelector('.header__burger');
     this.nav = document.querySelector('.header__nav');
+    this.navListNode = document.querySelector('.header__nav-list');
+    this.searchBlockNode = document.querySelector('.header__search');
+    this.openSearchNode = document.querySelector('.header__search-open');
+    this.closeSearchNode = document.querySelector('.header__search-close');
+    this.inputSearchNode = document.querySelector('.header__search-input');
+    this.startSearchNode = document.querySelector('.header__search-start');
+    this.drops = document.querySelectorAll('.header__nav-drop');
     this.init()
   }
   init() {
@@ -11,6 +18,9 @@ export default class Header {
     this.toggleHandler()
     this.setTopPadding()
     this.sizeHandler()
+    this.searchOpenHandler()
+    this.searchCloseHandler()
+    this.searchInputHandler()
   }
   open() {
     this.burger.classList.add('active')
@@ -23,24 +33,62 @@ export default class Header {
     document.body.classList.remove('lock')
   }
   toggleHandler() {
-    this.burger.addEventListener('click', () => {
+    this.burger ? this.burger.addEventListener('click', () => {
       if (this.burger.classList.contains('active')) {
         this.close()
       } else {
         this.open()
       }
-    })
+    }) : null
   }
   setTopPadding() {
     if (window.matchMedia("(max-width: 768px)").matches) {
       this.nav.style.paddingTop = this.header.clientHeight + 20 + 'px'
     } else {
       this.nav.style.paddingTop = 0
+      this.drops.forEach(i => {
+        i.style.top = this.header.clientHeight + 'px'
+      })
     }
   }
   sizeHandler() {
     window.addEventListener('resize', () => {
       this.setTopPadding()
     })
+  }
+  openSearch() {
+    this.navListNode.classList.add('hide')
+    this.searchBlockNode.classList.add('active')
+    this.inputSearchNode.focus()
+  }
+  closeSearch() {
+    this.navListNode.classList.remove('hide')
+    this.searchBlockNode.classList.remove('active')
+  }
+  searchOpenHandler() {
+    this.openSearchNode ? this.openSearchNode.addEventListener('click', () => {
+      this.openSearch()
+    }) : null
+  }
+  searchCloseHandler() {
+    this.closeSearchNode ? this.closeSearchNode.addEventListener('click', () => {
+      this.closeSearch()
+    }) : null
+  }
+  searchInputHandler() {
+    this.inputSearchNode ? this.inputSearchNode.addEventListener('keypress', (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        this.search()
+      }
+    }) : null
+    this.startSearchNode ? this.startSearchNode.addEventListener('click', (event) => {
+      this.search()
+    }) : null
+  }
+  search() {
+    const page = this.inputSearchNode.dataset.page
+    const newHref = window.location.origin + page + this.inputSearchNode.value
+    window.location.href = newHref
   }
 }
