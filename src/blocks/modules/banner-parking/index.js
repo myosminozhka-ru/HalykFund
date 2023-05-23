7159504fc399a987ef7e2c62f42b717f47eb18b4
arsosmi <sheraliev@myosminozhka.ru>
2023-05-23 16:33:10 +0600

    fix

--- src/blocks/modules/banner-parking/index.js
+++ src/blocks/modules/banner-parking/index.js
@@ -1,11 +1,9 @@
 export default class BannerParking {
   constructor() {
-    var image = document.getElementsByClassName('thumbnail');
+    var image = document.getElementsByClassName('rellax');
   
     if (image.length === 0) return
-    new simpleParallax(image, {
-      delay: .6,
-      transition: 'cubic-bezier(0,0,0,1)'
-    });
+
+    var rellax = new Rellax('.rellax');
   }
 }
